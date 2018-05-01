86455dd07f1b4bf2e1bd27bcc3d5daa1a0d8cd00
Christina Moore <sayhitochristina@gmail.com>
2018-05-01 16:04:09 -0500

    set up actions for markers

---
+++ src/actions/markerActions.js
@@ -0,0 +1,77 @@
+import { API_BASE_URL } from '../config'
+
+export const GET_MARKER_SUCCESS = 'GET_MARKER_SUCCESS';
+export const getMarkerSuccess = marker => ({
+  type: GET_MARKER_SUCCESS,
+  marker
+});
+
+export const GET_MARKER_ERROR = 'GET_MARKER_ERROR';
+export const getMarkerError = error => ({
+  type: GET_MARKER_ERROR,
+  error
+});
+
+export const GET_MARKER_REQUEST = 'GET_MARKER_REQUEST';
+export const getMarkerRequest = () => ({
+  type: GET_MARKER_REQUEST,
+});
+
+export const NEW_MARKER_SUCCESS = 'NEW_MARKER_SUCCESS';
+export const newMarkerSuccess = marker => ({
+  type: NEW_MARKER_SUCCESS,
+  marker
+});
+
+export const NEW_MARKER_ERROR = 'NEW_MARKER_ERROR';
+export const newMarkerError = error => ({
+  type: NEW_MARKER_ERROR,
+  error
+});
+
+export const NEW_MARKER_REQUEST = 'NEW_MARKER_REQUEST';
+export const newMarkerRequest = () => ({
+  type: NEW_MARKER_REQUEST,
+});
+
+export const newMarker = (incidentType, location, date, userId, description) => (dispatch, getState) => {
+  // TODO: once auth is set
+  // const authToken = getState().auth.authToken;
+  dispatch(newMarkerRequest())
+  return fetch(`${API_BASE_URL}/new/marker`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      // Authorization: `Bearer ${authToken}`
+    },
+    body: JSON.stringify({
+      incidentType,
+      location,
+      date,
+      userId,
+      description
+    })
+  })
+    .then(res => res.json())
+    .then(({ data }) => {
+      dispatch(newMarkerSuccess(data))
+      getMarkers()
+    })
+    .catch(err => {
+      dispatch(newMarkerError(err));
+    });
+};
+
+
+
+export const getMarkers = () => (dispatch, getState) => {
+  dispatch(getMarkerRequest())
+  return fetch(`${API_BASE_URL}/markers`, {
+    method: 'GET',
+  })
+    .then(res => res.json())
+    .then((data) => dispatch(getMarkerSuccess(data)))
+    .catch(err => {
+      dispatch(getMarkerError(err));
+    });
+}
