89cb65a0f33b6b6d2b06158134cfa72283906b3e
Adrian <adriantoddross@gmail.com>
2018-05-01 15:49:48 -0400

    create static form

--- src/components/report.js
+++ src/components/report.js
@@ -3,7 +3,54 @@ import React from 'react';
 export default function report(props) {
   return(
     <div>
-      <h1>Hello, world!</h1>
+      <form id="incident-report">
+        <label
+          htmlFor="incident-type">Incident Type
+        </label>
+        <select
+          id="incident-type"
+          name="type"
+          required="required">
+          <option value=""></option>
+          <option value="crime">Crime</option>
+          <option value="accident">Traffic Accident</option>
+          <option value="other">Other</option>
+        </select>
+        <label
+          htmlFor="incident-date">
+          Date
+        </label>
+        <input
+          type="date"
+          name="date"
+          id="incident-date"/>
+        <label
+          htmlFor="incident-time">
+          Time
+        </label>
+        <input
+          type="time"
+          name="time"
+          id="incident-time"/>
+        <label htmlFor="incident-desc">Description of Incident</label>
+        <textarea
+          name="description"
+          id="incident-desc"
+          cols="30"
+          rows="10">
+        </textarea>
+        <label htmlFor="incident-susp">Description of Suspect</label>
+        <textarea
+          name="suspect"
+          id="incident-desc"
+          cols="30"
+          rows="10">
+        </textarea>
+        <button
+          type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
