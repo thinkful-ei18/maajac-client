635b46f883b000b5444d442efdc15bb46cc26df2
Adrian <adriantoddross@gmail.com>
2018-05-01 16:49:52 -0400

    grab default export from report component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import {reportForm as Report} from './components/report';
+import reportForm from './components/report';
 // import GoogleApiWrapper from './components/GoogleApiWrapper';
 
 
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Report/>
+        <reportForm/>
         {/* <GoogleApiWrapper />  */}
       </div>
     );
--- src/components/report.js
+++ src/components/report.js
@@ -1,63 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Input from './input';
-import {connect} from 'react-redux';
+// import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form';
 
-export function reportForm(props) {
-  return(
-    <div>
-      <form id="incident-report">
-        <label
-          htmlFor="incident-type">Incident Type
-        </label>
-        <select
+class reportForm extends Component {
+  render() {
+    // const { handleSubmit } = this.props;
+
+    return(
+      <div>
+        <Field
+          component={Input}
           id="incident-type"
+          label="Incident Type"
           name="type"
-          required="required">
-          <option value=""></option>
-          <option value="crime">Crime</option>
-          <option value="accident">Traffic Accident</option>
-          <option value="other">Other</option>
-        </select>
-        <label
-          htmlFor="incident-date">
-          Date
-        </label>
-        <input
-          type="date"
-          name="date"
-          id="incident-date"/>
-        <label
-          htmlFor="incident-time">
-          Time
-        </label>
-        <input
-          type="time"
-          name="time"
-          id="incident-time"/>
-        <label htmlFor="incident-desc">Description of Incident</label>
-        <textarea
-          name="description"
-          id="incident-desc"
-          cols="30"
-          rows="10">
-        </textarea>
-        <label htmlFor="incident-susp">Description of Suspect</label>
-        <textarea
-          name="suspect"
-          id="incident-desc"
-          cols="30"
-          rows="10">
-        </textarea>
-        <button
-          type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+        />
+        <form id="incident-report">
+          <label
+            htmlFor="incident-type">Incident Type
+          </label>
+          <select
+            id="incident-type"
+            name="type"
+            required="required">
+            <option value=""></option>
+            <option value="crime">Crime</option>
+            <option value="accident">Traffic Accident</option>
+            <option value="other">Other</option>
+          </select>
+          <label
+            htmlFor="incident-date">
+            Date
+          </label>
+          <input
+            type="date"
+            name="date"
+            id="incident-date"/>
+          <label
+            htmlFor="incident-time">
+            Time
+          </label>
+          <input
+            type="time"
+            name="time"
+            id="incident-time"/>
+          <label htmlFor="incident-desc">Description of Incident</label>
+          <textarea
+            name="description"
+            id="incident-desc"
+            cols="30"
+            rows="10">
+          </textarea>
+          <label htmlFor="incident-susp">Description of Suspect</label>
+          <textarea
+            name="suspect"
+            id="incident-desc"
+            cols="30"
+            rows="10">
+          </textarea>
+          <button
+            type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default reduxForm({
+reportForm = reduxForm({
   form: "report"
 })(reportForm);
+
+export default reportForm;
