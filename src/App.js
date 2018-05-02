c6cb2b189faa189b3f0144448968a5fbe6a87663
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-02 14:07:07 -0700

    Router fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import ReportForm from './components/report';
