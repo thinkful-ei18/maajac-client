65c197d413aff6565630dff9cfa5f6bb2187f13b
Adrian <adriantoddross@gmail.com>
2018-05-02 16:42:30 -0400

    render registration form for testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 import ReportForm from './components/report';
+import RegistrationForm from './components/register';
 import GoogleMapWrapper from './components/GoogleMapWrapper';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <RegistrationForm/>
         {/* <ReportForm /> */}
         <GoogleMapWrapper />
       </div>
