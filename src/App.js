d50d08ce1d02a4edc7dc4ffbef519b019fe5a323
Christina Moore <sayhitochristina@gmail.com>
2018-05-02 14:34:12 -0500

    set up navbar and styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 import ReportForm from './components/report';
 import GoogleMapWrapper from './components/GoogleMapWrapper';
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         {/* <ReportForm /> */}
         <GoogleMapWrapper />
       </div>
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// styles
+import './css/navbar.css'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div className='navbar'>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
