e421558f1b67dc3ca4c78d9cd4e1f9fac7a9940c
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-02 09:22:08 -0700

    New googlemapwrapper componenet working

---
+++ src/components/GoogleMapWrapper.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { compose, withProps } from 'recompose';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.isMarkerShown && (
+      <Marker
+        position={{ lat: -34.397, lng: 150.644 }}
+        onClick={props.onMarkerClick}
+      />
+    )}
+    {console.log(props.onMarkerClick)}
+  </GoogleMap>
+));
+
+export class MyFancyComponent extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMarkerShown: false
+    };
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true });
+    }, 3000);
+  };
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false });
+    this.delayedShowMarker();
+  };
+
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    );
+  }
+}
