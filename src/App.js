d01193d6815325c3683bf6121a657b4b3d3b031c
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-02 14:53:13 -0700

    Browser route not needed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import ReportForm from './components/report';
