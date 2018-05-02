cc5f65799fae7176becb72681024f7dcba562992
Adrian <adriantoddross@gmail.com>
2018-05-02 14:57:04 -0400

    test checkDate validation

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -7,10 +7,23 @@ export const minLength = min => value =>
     value && value.length < min ? `Must be ${min} characters or more` : undefined
 export const minLength120 = minLength(120)
 
+const tomorrowString = () => {
+    let today = new Date();
+    let dd = today.getDate() + 1;
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
 
-let futureDate = new Date();
-futureDate.setDate(futureDate.getDate() + 1);
-// console.log('tomorrow:', futureDate);
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    return `${yyyy}-${mm}-${dd}`;
+};
+
+
 export const checkDate = value =>
-    value >= futureDate ? `You can't report an incident in the future!` : undefined;
-
+    value >= tomorrowString() ? `You can't report an incident in the future!` : undefined;
