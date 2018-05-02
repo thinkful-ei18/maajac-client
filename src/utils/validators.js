a1c07d6c759d105689e83919dcfb6cabf1ffe4f4
Adrian <adriantoddross@gmail.com>
2018-05-02 14:19:51 -0400

    add checkDate validation; not used yet!

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -7,10 +7,10 @@ export const minLength = min => value =>
     value && value.length < min ? `Must be ${min} characters or more` : undefined
 export const minLength120 = minLength(120)
 
+
 let futureDate = new Date();
 futureDate.setDate(futureDate.getDate() + 1);
-export const notTomorrow = date => date >= futureDate ? undefined : `You can't report an incident in the future!`;
-
-// export const numbersOnly = value =>
-//     /.*?(\d)[^\d]*/g.test(value) ? undefined : 'Must be a number';
+// console.log('tomorrow:', futureDate);
+export const checkDate = value =>
+    value >= futureDate ? `You can't report an incident in the future!` : undefined;
 
