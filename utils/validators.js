da661045e87eed45d75a2e52281cc7b85e56dc84
Adrian <adriantoddross@gmail.com>
2018-05-02 12:16:28 -0400

    move validators into source folder

--- utils/validators.js
+++
@@ -1,8 +0,0 @@
-export const required = value => (value ? undefined : 'Required');
-
-export const nonEmpty = value =>
-    value.trim() !== '' ? undefined : 'Cannot be empty';
-
-// export const numbersOnly = value =>
-//     /.*?(\d)[^\d]*/g.test(value) ? undefined : 'Must be a number';
-
