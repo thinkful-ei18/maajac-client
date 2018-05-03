6d06b81d95e110d559aa08755088307a8bce5ad9
Christina Moore <sayhitochristina@gmail.com>
2018-05-03 11:13:35 -0500

    create dropdown menu

--- src/components/mapStyle.js
+++ src/components/mapStyle.js
@@ -1,169 +1,137 @@
 export const styles = [
-  { featureType: 'all', elementType: 'all', stylers: [{ visibility: 'on' }] },
   {
-    featureType: 'all',
-    elementType: 'labels',
-    stylers: [{ visibility: 'off' }, { saturation: '-100' }]
-  },
-  {
-    featureType: 'all',
-    elementType: 'labels.text.fill',
-    stylers: [
-      { saturation: 36 },
-      { color: '#000000' },
-      { lightness: 40 },
-      { visibility: 'off' }
+    "featureType": "landscape.man_made",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f7f1df"
+      }
     ]
   },
   {
-    featureType: 'all',
-    elementType: 'labels.text.stroke',
-    stylers: [{ visibility: 'off' }, { color: '#000000' }, { lightness: 16 }]
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#d0e3b4"
+      }
+    ]
   },
   {
-    featureType: 'all',
-    elementType: 'labels.icon',
-    stylers: [{ visibility: 'off' }]
+    "featureType": "landscape.natural.terrain",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
   },
   {
-    featureType: 'administrative',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#000000' }, { lightness: 20 }]
+    "featureType": "poi",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
   },
   {
-    featureType: 'administrative',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#000000' }, { lightness: 17 }, { weight: 1.2 }]
+    "featureType": "poi.business",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
   },
   {
-    featureType: 'landscape',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 20 }]
+    "featureType": "poi.medical",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fbd3da"
+      }
+    ]
   },
   {
-    featureType: 'landscape',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#4d6059' }]
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#bde6ab"
+      }
+    ]
   },
   {
-    featureType: 'landscape',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#4d6059' }]
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
   },
   {
-    featureType: 'landscape.natural',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#4d6059' }]
-  },
-  { featureType: 'poi', elementType: 'geometry', stylers: [{ lightness: 21 }] },
-  {
-    featureType: 'poi',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#4d6059' }]
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
   },
   {
-    featureType: 'poi',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#4d6059' }]
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#ffe15f"
+      }
+    ]
   },
   {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [{ visibility: 'on' }, { color: '#7f8d89' }]
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#efd151"
+      }
+    ]
   },
   {
-    featureType: 'road',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#7f8d89' }]
+    "featureType": "road.arterial",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
   },
   {
-    featureType: 'road.highway',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#7f8d89' }, { lightness: 17 }]
+    "featureType": "road.local",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "black"
+      }
+    ]
   },
   {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#7f8d89' }, { lightness: 29 }, { weight: 0.2 }]
+    "featureType": "transit.station.airport",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#cfb2db"
+      }
+    ]
   },
   {
-    featureType: 'road.arterial',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 18 }]
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#7f8d89' }]
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#7f8d89' }]
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 16 }]
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#7f8d89' }]
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#7f8d89' }]
-  },
-  {
-    featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [{ color: '#000000' }, { lightness: 19 }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'all',
-    stylers: [{ color: '#2b3638' }, { visibility: 'on' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [{ color: '#2b3638' }, { lightness: 17 }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry.fill',
-    stylers: [{ color: '#24282b' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#24282b' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels',
-    stylers: [{ visibility: 'off' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text',
-    stylers: [{ visibility: 'off' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [{ visibility: 'off' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [{ visibility: 'off' }]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.icon',
-    stylers: [{ visibility: 'off' }]
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#a2daf2"
+      }
+    ]
   }
-];
+]
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+// import Menu from '../components/dropdownMenu';
 
 // styles
 import './css/navbar.css'
 
 export class Navbar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false,
+    };
+
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu() {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu);
+    });
+  }
+
   render() {
     return (
-      <nav className='navbar'>
-        <span className='home'><NavLink to='/'>GuardDog</NavLink></span>
-        <NavLink to='/submit'>Submit new report</NavLink>
-      </nav>
-    )
+      <div>
+        <nav className='navbar'>
+          <span className='home'><NavLink to='/'>App Title</NavLink></span>
+
+
+          <button onClick={this.showMenu}>
+            Show menu
+        </button>
+
+          {
+            this.state.showMenu
+              ? (
+                <div className="dropdown-menu">
+                  <NavLink to='/submit'>Submit new report</NavLink>
+                  <button> Dummy link 1 </button>
+                  <button> Dummy link 2 </button>
+                </div>
+              )
+              : (
+                null
+              )
+          }
+        </nav>
+      </div>
+    );
   }
 }
+
 
 export default Navbar;
