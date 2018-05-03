40547a0b5baeb9dbea2c4a9f5892a01e2d3137bb
alishaevn <alieva.inc@gmail.com>
2018-05-02 17:39:45 -0700

    add width style for display purposes

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -14,7 +14,7 @@ const GoogleMapComponent = compose(
     googleMapURL:
       'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100vh` }} />,
+    containerElement: <div style={{ height: `100vh`, width: `100vw` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
