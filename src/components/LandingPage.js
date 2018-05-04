19691ef4ac4b81608bf0e47264fb3e3eaa7d57c0
Adrian <adriantoddross@gmail.com>
2018-05-04 17:17:36 -0400

    add modal to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,13 +5,34 @@ import GoogleMapWrapper from './GoogleMapWrapper';
 import ReportForm from './report';
 import Dialog from 'material-ui/Dialog';
 import { closeDialog } from '../actions/modalActions';
+import LoginForm from '../components/Login';
+import RegistrationForm from '../components/register';
 
 import './css/landingPage.css'
 
 export class LandingPage extends Component {
+
+  handleCloseDialog() {
+		console.log('closing dialog...');
+		this.props.dispatch(closeDialog())
+  }
+
   render() {
+
+    let modalForm;
+    if (this.props.currentTab) {
+			if (this.props.currentTab === "signup") {
+				modalForm = <RegistrationForm/>
+			} else {
+				modalForm = <LoginForm/>
+			}
+		}
+
     return (
       <div className="landing-page">
+        <Dialog title="App Name" modal={false} autoScrollBodyContent={true} open={this.props.dialog} onRequestClose={() => this.handleCloseDialog()}>
+					{modalForm}
+				</Dialog>
 
         <ReportForm />
         <GoogleMapWrapper />
