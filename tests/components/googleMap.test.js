e3b431a72f9750ebe8649cb56a3af7ca9b3f1375
Christina Moore <sayhitochristina@gmail.com>
2018-05-04 13:20:28 -0500

    start tests

---
+++ tests/components/googleMap.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import GoogleMapComponent from '../../src/components/googleMap';
+
+describe('<Home />', () => {
+  it('renders without crashing', () => {
+    shallow(<GoogleMapComponent />)
+  })
+})
