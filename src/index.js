72457fa55f0af2dca5ebe6ad8bd8464281677c91
Adrian <adriantoddross@gmail.com>
2018-05-04 17:20:40 -0400

    wrap app in Mui Theme provider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import store from './store';
+
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
     </Router>
   </Provider>,
   document.getElementById('root')
