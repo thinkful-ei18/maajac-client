04f81bdaf6694f9c08065d854e6c11249cb6827e
Christina Moore <sayhitochristina@gmail.com>
2018-05-04 15:36:14 -0500

    user auth reducer tests

--- src/tests/reportReducer.test.js
+++ src/tests/reportReducer.test.js
@@ -1,4 +1,5 @@
 import { reportReducer } from "../reducers/reportReducer";
+import { setUserLocationSuccess, setUserLocationError, setUserLocationRequest } from "../actions/reportActions";
 
 
 describe('report reducer', () => {
@@ -8,6 +9,45 @@ describe('report reducer', () => {
       userLocation: {},
       loading: false,
       error: null,
+    })
+  })
+
+  it('should return the state when an unknown action is passed', () => {
+    let currentState = {}
+    const state = reportReducer(currentState, { type: '@@UNKNOWN' })
+    expect(state).toBe(currentState)
+  })
+
+  it('should set the user location', () => {
+    const location = { location: '123 Avenue Drive' }
+    let state;
+    state = reportReducer(state, setUserLocationSuccess(location))
+    expect(state).toEqual({
+      ...state,
+      userLocation: location,
+      loading: false,
+      error: false
+    })
+  })
+
+  it('should set the error', () => {
+    const err = 'error'
+    let state;
+    state = reportReducer(state, setUserLocationError(err))
+    expect(state).toEqual({
+      ...state,
+      loading: false,
+      error: err
+    })
+  })
+
+  it('should set the request', () => {
+    let state;
+    state = reportReducer(state, setUserLocationRequest())
+    expect(state).toEqual({
+      ...state,
+      loading: true,
+      error: false
     })
   })
 })
---
+++ src/tests/userAuthReducer.test.js
@@ -0,0 +1,82 @@
+import { userAuthReducer } from "../reducers/userAuthReducer";
+import { setAuthToken, clearAuth, authRequest, authSuccess, authError, registerError } from "../actions/userActions";
+
+describe('userAuthReducer reducer', () => {
+  it('should return the intial state', () => {
+    const state = userAuthReducer(undefined, { type: '@@TEST' });
+    expect(state).toEqual({
+      authToken: null,
+      currentUser: null,
+      loading: false,
+      error: null,
+    })
+  })
+
+  it('should return the state with an unknown action', () => {
+    let currentState = {}
+    const state = userAuthReducer(currentState, { type: '@@UNKNOWN' })
+    expect(state).toBe(currentState)
+  })
+
+  it('should set the authToken', () => {
+    const authToken = 12345
+    let state;
+    state = userAuthReducer(state, setAuthToken(authToken))
+    expect(state).toEqual({
+      ...state,
+      authToken: authToken
+    })
+  })
+
+  it('should clear the authToken', () => {
+    let state;
+    state = userAuthReducer(state, clearAuth())
+    expect(state).toEqual({
+      ...state,
+      authToken: null,
+      currentUser: null
+    })
+  })
+
+  it('should set loading to true', () => {
+    let state;
+    state = userAuthReducer(state, authRequest())
+    expect(state).toEqual({
+      ...state,
+      loading: true
+    })
+  })
+
+  it('should set the currentUser and loading false', () => {
+    const user = 'Michael Scott'
+    let state;
+    state = userAuthReducer(state, authSuccess(user))
+    expect(state).toEqual({
+      ...state,
+      currentUser: user,
+      loading: false
+    })
+  })
+
+  it('should set the error', () => {
+    const err = 'bee boop error'
+    let state;
+    state = userAuthReducer(state, authError(err))
+    expect(state).toEqual({
+      ...state,
+      error: err,
+      loading: false
+    })
+  })
+
+  it('should set the error on register error', () => {
+    const err = 'error error';
+    let state;
+    state = userAuthReducer(state, registerError(err))
+    expect(state).toEqual({
+      ...state,
+      error: err,
+      loading: false
+    })
+  })
+})
