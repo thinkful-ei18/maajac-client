79422d3f583c77de5d3a251de83ea57a7e22f6a3
Adrian <adriantoddross@gmail.com>
2018-05-04 14:45:11 -0400

    redirect user to dashboard after logging in

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,7 +18,7 @@ export class LoginForm extends Component {
 
 		return (
 			<div className='login'>
-				{this.props.loggedIn ? (<Redirect to='/' />) : ''}
+				{this.props.loggedIn ? (<Redirect to='/dashboard' />) : ''}
 				<form
 					className="login-form"
 					onSubmit={handleSubmit(values => {
