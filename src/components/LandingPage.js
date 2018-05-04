b24ad0b250a77577bf5f05b0f6ece4852a4c619b
Adrian <adriantoddross@gmail.com>
2018-05-04 17:04:21 -0400

    import Dialog, closeDialog then mapStatetoProps/connect the component

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux"
 
 import GoogleMapWrapper from './GoogleMapWrapper';
 import ReportForm from './report';
+import Dialog from 'material-ui/Dialog';
+import { closeDialog } from '../actions/modalActions';
 
 import './css/landingPage.css'
 
-export default class LandingPage extends Component {
+export class LandingPage extends Component {
   render() {
     return (
       <div className="landing-page">
+
         <ReportForm />
         <GoogleMapWrapper />
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+	currentTab: state.modal.currentTab,
+	dialog: state.modal.dialog,
+	currentUser: state.auth.currentUser ? state.auth.currentUser : ''
+})
+
+export default connect(mapStateToProps)(LandingPage);
