8e2ab4b549f56c8798f0e78c071a831c559b9064
Adrian <adriantoddross@gmail.com>
2018-05-07 13:46:22 -0400

    add user credential actions

--- src/local-storage.js
+++ src/local-storage.js
@@ -13,3 +13,16 @@ export const clearAuthToken = () => {
         localStorage.removeItem('authToken');
     } catch (e) {}
 };
+
+/* ========== USER CREDENTIALS ========== */
+export const saveUserCredentials = user => {
+    try {
+        localStorage.setItem('user', JSON.stringify(user));
+    } catch (e) {}
+}
+
+export const clearUserCredentials = () => {
+    try {
+        localStorage.removeItem('user');
+    } catch (e) {}
+};
