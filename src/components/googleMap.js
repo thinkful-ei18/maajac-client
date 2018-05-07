7b176415977e3e7252f9e5bf276022b8edeb5ecd
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-07 14:38:32 -0700

    svg rendering

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -32,7 +32,17 @@ const GoogleMapComponent = compose(
       props.markers.map((marker, index) => {
         return <Incident marker={marker} key={index} />;
       })}) )
-    <Marker position={props.indicatorPin} onClick={props.onToggleOpen}>
+    <Marker
+      position={props.indicatorPin}
+      icon={{
+        path:
+          'M256,0C167.641,0,96,71.625,96,160c0,24.75,5.625,48.219,15.672,69.125C112.234,230.313,256,512,256,512l142.594-279.375 C409.719,210.844,416,186.156,416,160C416,71.625,344.375,0,256,0z M256,256c-53.016,0-96-43-96-96s42.984-96,96-96   c53,0,96,43,96,96S309,256,256,256z',
+        fillColor: '#D80027',
+        fillOpacity: 0.8,
+        scale: 1
+      }}
+      onClick={props.onToggleOpen}
+    >
       {props.isOpen && (
         <InfoWindow onCloseClick={props.onToggleOpen}>
           <p>Incident Marker</p>
