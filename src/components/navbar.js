fb0ce67aef87f3ff425a2e8093e104175da341c5
Christina Moore <sayhitochristina@gmail.com>
2018-05-07 13:41:55 -0500

    add map and report link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { clearAuth } from '../actions/userActions';
 import {
   openSignUp,
@@ -61,7 +61,7 @@ export class Navbar extends Component {
 
     if (this.props.loggedIn) {
       loggedInNavbar = (
-        <button className="report-button" onClick={this.showMenu}>
+        <button className="report-button-large" onClick={this.showMenu}>
           Welcome, {this.props.currentUser.username}
         </button>
       );
@@ -87,8 +87,10 @@ export class Navbar extends Component {
           {loggedInNavbar}
           {this.state.showMenu ? (
             <div className="dropdown-menu">
-              <NavLink to="/dashboard">Dashboard</NavLink>
-              <button className="report-button" onClick={this.logout}>
+              <Link to="/">Map</Link>
+              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/" className='mobile-only'>Report</Link>
+              <button className="navbar-link" onClick={this.logout}>
                 {' '}
                 Logout{' '}
               </button>
