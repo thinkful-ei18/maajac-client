5cd506fc5575911bb9658778ea082df90cc7b33e
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-08 08:02:25 -0700

    scratch js

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -10,6 +10,7 @@ import {
 } from 'react-google-maps';
 import Incident from './IncidentMarker';
 import { styles } from './mapStyle';
+import image from '../images/map-marker.svg';
 
 const GoogleMapComponent = compose(
   withProps({
@@ -35,11 +36,8 @@ const GoogleMapComponent = compose(
     <Marker
       position={props.indicatorPin}
       icon={{
-        path:
-          'M256,0C167.641,0,96,71.625,96,160c0,24.75,5.625,48.219,15.672,69.125C112.234,230.313,256,512,256,512l142.594-279.375 C409.719,210.844,416,186.156,416,160C416,71.625,344.375,0,256,0z M256,256c-53.016,0-96-43-96-96s42.984-96,96-96   c53,0,96,43,96,96S309,256,256,256z',
-        fillColor: '#D80027',
-        fillOpacity: 1,
-        scale: 0.1
+        url: image,
+        scaledSize: new maps.Size(40, 60)
       }}
       onClick={props.onToggleOpen}
     >
