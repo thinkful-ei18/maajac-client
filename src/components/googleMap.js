9d088778cc251789f53325ee181a8ae118191355
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-09 12:07:45 -0700

    pushing branch

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,6 +1,6 @@
-
+import _ from 'lodash';
 import React from 'react';
-import { compose, withProps } from 'recompose';
+import { compose, withProps, lifecycle } from 'recompose';
 import {
   withScriptjs,
   withGoogleMap,
@@ -9,13 +9,16 @@ import {
   InfoWindow
 } from 'react-google-maps';
 
+import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
+
 import Incident from './IncidentMarker';
 import { styles } from './mapStyle';
 
 import image from '../images/map-marker.svg';
 
-const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
-
+const {
+  MarkerClusterer
+} = require('react-google-maps/lib/components/addons/MarkerClusterer');
 
 const GoogleMapComponent = compose(
   withProps({
@@ -25,9 +28,61 @@ const GoogleMapComponent = compose(
     containerElement: <div style={{ height: `100%`, width: `100vw` }} />,
     mapElement: <div style={{ height: `93vh` }} />
   }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {};
+
+      this.setState({
+        bounds: null,
+        center: {
+          lat: 41.9,
+          lng: -87.624
+        },
+        markers: [],
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        onBoundsChanged: () => {
+          this.setState({
+            bounds: refs.map.getBounds(),
+            center: refs.map.getCenter()
+          });
+        },
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+          const bounds = new google.maps.LatLngBounds();
+
+          places.forEach(place => {
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          const nextMarkers = places.map(place => ({
+            position: place.geometry.location
+          }));
+          const nextCenter = _.get(
+            nextMarkers,
+            '0.position',
+            this.state.center
+          );
+
+          this.setState({
+            center: nextCenter,
+            markers: nextMarkers
+          });
+          // refs.map.fitBounds(bounds);
+        }
+      });
+    }
+  }),
   withScriptjs,
   withGoogleMap
-)(props =>
+)(props => (
   <GoogleMap
     ref={props.onMapMounted}
     onBoundsChanged={props.onBoundsChanged}
@@ -36,7 +91,29 @@ const GoogleMapComponent = compose(
     onClick={props.onHandleClick}
     defaultOptions={{ styles }}
   >
-
+    <SearchBox
+      ref={props.onSearchBoxMounted}
+      controlPosition={1}
+      onPlacesChanged={props.onPlacesChanged}
+    >
+      <input
+        type="text"
+        placeholder="Search"
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `32px`,
+          marginTop: `27px`,
+          padding: `0 12px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`
+        }}
+      />
+    </SearchBox>
     {/* Marker that user drops */}
     <Marker
       position={props.indicatorPin}
@@ -46,7 +123,6 @@ const GoogleMapComponent = compose(
       }}
       onClick={props.onToggleOpen}
     >
-
       {props.isOpen && (
         <InfoWindow onCloseClick={props.onToggleOpen}>
           <p>Incident Marker</p>
@@ -67,11 +143,9 @@ const GoogleMapComponent = compose(
       {props.isMarkerShown &&
         props.markers.map((marker, index) => {
           return <Incident marker={marker} key={index} />;
-        })
-      }
-
+        })}
     </MarkerClusterer>
   </GoogleMap>
-);
+));
 
 export default GoogleMapComponent;
