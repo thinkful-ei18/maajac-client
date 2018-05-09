7b1f46b5c19369cc5b19291c0e36c3d47e10ae34
alishaevn <alieva.inc@gmail.com>
2018-05-09 10:57:37 -0700

    map renders without recompose. can't add a marker though

--- src/components/GoogleMap2.js
+++ src/components/GoogleMap2.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-// import { compose, withProps } from 'recompose';
 import {
   withScriptjs,
   withGoogleMap,
@@ -79,67 +78,9 @@ const GoogleMapComponent = props => {
 
 }
 
-
-// const GoogleMapComponent = compose(
-//   withProps({
-//     googleMapURL:
-//       'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
-//     loadingElement: <div style={{ height: `100%` }} />,
-//     containerElement: <div style={{ height: `100%`, width: `100vw` }} />,
-//     mapElement: <div style={{ height: `93vh` }} />
-//   }),
-//   withScriptjs,
-//   withGoogleMap
-// )(props =>
-//   <GoogleMap
-//     ref={props.onMapMounted}
-//     onBoundsChanged={props.onBoundsChanged}
-//     defaultZoom={12}
-//     center={props.position}
-//     onClick={props.onHandleClick}
-//     defaultOptions={{ styles }}
-//   >
-
-//     {/* Marker that user drops */}
-//    <Marker
-//      position={props.indicatorPin}
-//      icon={{
-//        url: image,
-//        scaledSize: { width: 31, height: 43 }
-//      }}
-//      onClick={props.onToggleOpen}
-//    >
-
-//      {props.isOpen && (
-//        <InfoWindow onCloseClick={props.onToggleOpen}>
-//          <p>Incident Marker</p>
-//        </InfoWindow>
-//      )}
-//    </Marker> 
-
-    // {/* Marker cluster */}
-    // <MarkerClusterer
-    //   onClick={props.onMarkerClustererClick}
-    //   averageCenter
-    //   enableRetinaIcons
-    //   gridSize={30} // change for size of cluster area
-    //   maxZoom={15} // change how far map zooms when clicking cluster
-    //   defaultMinimumClusterSize={2} // mimimum cluster size
-    // >
-    //   {/* Populated markers */}
-    //   {props.isMarkerShown &&
-    //     props.markers.map((marker, index) => {
-    //       return <Incident marker={marker} key={index} />;
-    //     })
-    //   }
-
-    // </MarkerClusterer>
-//   </GoogleMap>
-// );
-
 export default GoogleMapComponent;
 
-/**
+/*
  Resources:
   - https://github.com/tomchentw/react-google-maps/issues/636
  */
