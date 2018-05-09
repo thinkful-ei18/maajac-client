4f38584c13b6313139e47382c717e231c224d517
Christina Moore <sayhitochristina@gmail.com>
2018-05-09 13:26:09 -0500

    add actions and reducers for modal

--- src/actions/modalActions.js
+++ src/actions/modalActions.js
@@ -17,3 +17,15 @@ export const CLOSE_DIALOG = "CLOSE_DIALOG"
 export const closeDialog = () => ({
 	type: CLOSE_DIALOG
 })
+
+// Profile picture modal
+
+export const PROFILE_OPEN_DIALOG = "PROFILE_OPEN_DIALOG"
+export const profileOpenDialog = () => ({
+	type: PROFILE_OPEN_DIALOG
+})
+
+export const PROFILE_CLOSE_DIALOG = "PROFILE_CLOSE_DIALOG"
+export const profileCloseDialog = () => ({
+	type: PROFILE_CLOSE_DIALOG
+})
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Dialog from 'material-ui/Dialog';
 
 import './css/userProfile.css';
+import { profileOpenDialog } from '../actions/modalActions';
 
 
-class UserProfile extends Component {
+export class UserProfile extends Component {
+
+  ppUploadModal() {
+    console.log('open modal')
+    this.props.dispatch(profileOpenDialog());
+  }
+
   render() {
-    return(
+    return (
       <section className='user-profile'>
 
         <div className='user-pic-div'>
-          <img src={require('../images/avatar.png')} alt='profile-pic' className='profile-pic'/>
+          <img src={require('../images/avatar.png')} alt='profile-pic' className='profile-pic' />
         </div>
-
+        <button className='report-button-large' onClick={() => this.ppUploadModal()}>Upload profile picture</button>
         <div className='user-info-div'>
           <p className='user username'>{this.props.currentUser.username}</p>
           <p className='user'>blahzay</p>
         </div>
+        <Dialog
+          bodyClassName="modal"
+          title="Profile Picture"
+          modal={false}
+          autoScrollBodyContent={true}
+          contentStyle={{ width: 300 }}
+          open={this.props.dialog}
+          onRequestClose={() => this.handleCloseDialog()}
+        >
+          <p>hi</p>
+        </Dialog>
       </section>
     )
   }
@@ -24,7 +43,8 @@ class UserProfile extends Component {
 
 export const mapStateToProps = (state, props) => ({
   // loggedIn: state.auth.currentUser !== null,
-  currentUser: state.auth.currentUser ? state.auth.currentUser : ''
+  currentUser: state.auth.currentUser ? state.auth.currentUser : '',
+  dialog: state.modal.ppModal
 });
 
 export default connect(mapStateToProps)(UserProfile);
