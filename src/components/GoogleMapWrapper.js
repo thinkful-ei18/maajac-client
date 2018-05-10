45133f772209ff80cecfdb4b5fd50fe75baaa502
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-10 12:24:27 -0700

    Map center changed

--- src/components/GoogleMapWrapper.js
+++ src/components/GoogleMapWrapper.js
@@ -7,90 +7,90 @@ import { setUserLocation } from '../actions/reportActions';
 import { setDefaultLocation } from '../actions/defaultLocationActions';
 
 export class GoogleMapWrapper extends React.PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isMarkerShown: true,
-			location: {
-				lat: -34.397,
-				lng: 150.644,
-			},
-			indicatorPin: {
-				lat: -34.397,
-				lng: 150.644,
-			},
-			popupIsOpen: false,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMarkerShown: true,
 
-	componentDidMount() {
-		// Get markers from server
-		this.props.dispatch(getMarkers());
+      location: {
+        lat: 40.650002,
+        lng: -73.94997
+      },
+      indicatorPin: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      popupIsOpen: false
+    };
+  }
 
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(
-				position => {
-					const userlocation = {
-						lat: position.coords.latitude,
-						lng: position.coords.longitude,
-					};
-					this.props.dispatch(setDefaultLocation({ userlocation }));
-					this.setState({
-						location: userlocation,
-					});
-				},
-				error => {
-					console.log(error);
-				}
-			);
-		}
-	}
+  componentDidMount() {
+    // Get markers from server
+    this.props.dispatch(getMarkers());
 
-	handleMapClick(event) {
-		let lat = event.latLng.lat();
-		let lng = event.latLng.lng();
-		console.log('lat:', lat);
-		console.log('lng', lng);
-		this.setState({
-			indicatorPin: {
-				lat,
-				lng,
-			},
-		});
-		// set the pin location in state
-		this.props.dispatch(setUserLocation({ lat, lng }));
-	}
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const userlocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          this.props.dispatch(setDefaultLocation({ userlocation }));
+          this.setState({
+            location: userlocation
+          });
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }
 
-	onToggleOpen() {
-		this.setState({
-			popupIsOpen: !this.state.popupIsOpen,
-		});
-	}
+  handleMapClick(event) {
+    let lat = event.latLng.lat();
+    let lng = event.latLng.lng();
+    console.log('lat:', lat);
+    console.log('lng', lng);
+    this.setState({
+      indicatorPin: {
+        lat,
+        lng
+      }
+    });
+    // set the pin location in state
+    this.props.dispatch(setUserLocation({ lat, lng }));
+  }
 
-	render() {
-		return (
-			<GoogleMapComponent
-				path={this.props.path}
-				isMarkerShown={this.state.isMarkerShown}
-				onMarkerClick={this.handleMarkerClick}
-				position={this.props.defaultLocation}
-				onHandleClick={e => this.handleMapClick(e)}
-				markers={this.props.markersFromServer}
-				indicatorPin={this.state.indicatorPin}
-				isOpen={this.state.popupIsOpen}
-				onToggleOpen={() => this.onToggleOpen()}
-			/>
-		);
-	}
+  onToggleOpen() {
+    this.setState({
+      popupIsOpen: !this.state.popupIsOpen
+    });
+  }
+
+  render() {
+    return (
+      <GoogleMapComponent
+        path={this.props.path}
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+        position={this.props.defaultLocation}
+        onHandleClick={e => this.handleMapClick(e)}
+        markers={this.props.markersFromServer}
+        indicatorPin={this.state.indicatorPin}
+        isOpen={this.state.popupIsOpen}
+        onToggleOpen={() => this.onToggleOpen()}
+      />
+    );
+  }
 }
 
 export const mapStateToProps = (state, props) => ({
-	markersFromServer: state.markers.allMarkers ? state.markers.allMarkers : [],
-	defaultLocation: state.defaultLocation.location,
+  markersFromServer: state.markers.allMarkers ? state.markers.allMarkers : [],
+  defaultLocation: state.defaultLocation.location
 });
 
 export default connect(mapStateToProps)(GoogleMapWrapper);
-
 
 /*
 Resources:
