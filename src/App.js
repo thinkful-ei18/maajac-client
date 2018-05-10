a6e992a422fe41f0ac08a8ae47099793f026ef6f
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-10 09:30:02 -0700

    search working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import RootPage from './components/RootPage';
 import LandingPage from './components/LandingPage';
 import Filter from './components/Filter';
 import RegistrationForm from './components/register';
@@ -13,19 +14,20 @@ import Search from './components/SearchBox';
 import './App.css';
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Navbar />
-				<Route exact path="/" component={LandingPage} />
-				<Route exact path="/register" component={RegistrationForm} />
-				<Route exact path="/login" component={LoginForm} />
-				<Route exact path="/report" component={LandingPage} />
-				<Route exact path="/dashboard" component={Dashboard} />
-				<Route exact path="/about" component={About} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={RootPage} />
+        <Route exact path="/map" component={LandingPage} />
+        <Route exact path="/register" component={RegistrationForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/report" component={LandingPage} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/about" component={About} />
+      </div>
+    );
+  }
 }
 
 export default App;
