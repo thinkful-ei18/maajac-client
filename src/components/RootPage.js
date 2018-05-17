10182451c91f645f277b23928bf62ec42f2fcce0
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-17 11:12:40 -0700

    redirect to map or report

--- src/components/RootPage.js
+++ src/components/RootPage.js
@@ -23,6 +23,35 @@ class RootPage extends Component {
 	}
 
 	render() {
+		function getOS() {
+			let userAgent = window.navigator.userAgent,
+				platform = window.navigator.platform,
+				macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+				windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+				iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+				os = null;
+
+			if (macosPlatforms.indexOf(platform) !== -1) {
+				os = 'Mac OS';
+			} else if (iosPlatforms.indexOf(platform) !== -1) {
+				os = 'iOS';
+			} else if (windowsPlatforms.indexOf(platform) !== -1) {
+				os = 'Windows';
+			} else if (/Android/.test(userAgent)) {
+				os = 'Android';
+			} else if (!os && /Linux/.test(platform)) {
+				os = 'Linux';
+			}
+
+			return os;
+		}
+		const os = getOS();
+		if (this.props.currentUser && (os === 'Mac Os' || 'Windows' || 'Linux')) {
+			return <Redirect to="/map" />;
+		} else if (this.props.currentUser && (os === 'iOS' || 'Android')) {
+			return <Redirect to="/report" />;
+		}
+
 		const linkStyle = {
 			color: 'white',
 			display: 'block',
